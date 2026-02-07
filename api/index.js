ad68bcf03584f823c420a6366f8b96d52999c53a
ikari4 <michael.ragusa@me.com>
2026-02-07 13:27:32 -0500

    project setup

---
+++ api/index.js
@@ -0,0 +1,9 @@
+// index.js
+
+import { createClient } from "@libsql/client";  
+
+const turso = createClient({
+  url: process.env.TURSO_DATABASE_URL,
+  authToken: process.env.TURSO_AUTH_TOKEN,
+});
+
